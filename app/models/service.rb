33157ac65e4eb15f28b65597d6d7281f571852e3
Cory Loken <cory@me.com>
2014-06-18 19:59:46 -0700

    Fixed bug with service and host name

--- app/models/service.rb
+++ app/models/service.rb
@@ -3,6 +3,8 @@ class Service  < ActiveRecord::Base
   include Nagios
 
   def activate *params
+    @host = Host.find host_id
+
     case name.downcase
       when 'http'
         write_http_service host_id
@@ -15,30 +17,30 @@ class Service  < ActiveRecord::Base
 
   protected
 
-  def write_http_service host_name
+  def write_http_service host_id
     doc = "
       define service{
         use                             generic-service         ; Name of service template to use
-        host_name                       #{host_name}
+        host_name                       #{@host.name}
         service_description             HTTP
         check_command                   check_http
         notifications_enabled           0
         }
     "
-    File.open("/usr/local/nagios/etc/objects/host_configs/#{host_name}_http_service.cfg", 'w') {|f| f.write(doc) }
+    File.open("/usr/local/nagios/etc/objects/host_configs/#{@host.name}_http_service.cfg", 'w') {|f| f.write(doc) }
   end
 
   def write_ping_service host_name
     doc = "
       define service{
         use                             generic-service         ; Name of service template to use
-        host_name                       #{host_name}
+        host_name                       #{@host.name}
         service_description             PING
         check_command                   check_ping!100.0,20%!500.0,60%
         notifications_enabled           0
         }
     "
-    File.open("/usr/local/nagios/etc/objects/host_configs/#{host_name}_ping_service.cfg", 'w') {|f| f.write(doc) }
+    File.open("/usr/local/nagios/etc/objects/host_configs/#{@host.name}_ping_service.cfg", 'w') {|f| f.write(doc) }
   end
 
 end
